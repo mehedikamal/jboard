c435093df115685f0ac08725aeab2e8519fd3f6b
jmorr122 <john.spencer.morris@gmail.com>
2015-03-28 20:48:38 -0400

    Markup for global header file.

--- app/views/header.php
+++ app/views/header.php
@@ -1,8 +1,19 @@
 <?php
-require_once 'braintree-php-2.37.0/lib/Braintree.php';
+//require_once 'braintree-php-2.37.0/lib/Braintree.php';
 
-Braintree_Configuration::environment('sandbox');
-Braintree_Configuration::merchantId('5sh4xwfdxphyt3g6');
-Braintree_Configuration::publicKey('jf3qdyp8mnhwt7xn');
-Braintree_Configuration::privateKey('4855a3314bc05b6ac9ed619e0baea08a');
+// Braintree_Configuration::environment('sandbox');
+// Braintree_Configuration::merchantId('5sh4xwfdxphyt3g6');
+// Braintree_Configuration::publicKey('jf3qdyp8mnhwt7xn');
+// Braintree_Configuration::privateKey('4855a3314bc05b6ac9ed619e0baea08a');
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Job Board for Remote Development Positions</title>
+  <meta name='description' content='Job Board for Remote Development Positions' />
+
+  <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' />
+  <?php echo HTML::style('css/style.css'); ?>
+</head>
+<body>

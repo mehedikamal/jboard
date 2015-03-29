06c064d62938b7604b5e487aa5a1d10eed354e00
Stephen Hutchens <sbrhutchens@aol.com>
2015-03-28 21:36:30 -0400

    setting up the layout structure

--- app/views/checkout.php
+++ app/views/checkout.php
@@ -22,42 +22,8 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
+<?php include('includes/header.php'); ?>
 
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
-
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
-</head>
-<body>
 	<div class="welcome">
 		<a href="http://laravel.com" title="Laravel PHP Framework">
 
@@ -68,5 +34,5 @@
 			echo 'checkout';
 		?>
 	</div>
-</body>
-</html>
+
+<?php include('includes/header.php'); ?>
---
+++ app/views/includes/footer.php
@@ -0,0 +1,7 @@
+      <footer class="footer">
+        <p>No Office Needed &copy; <?php echo date('Y'); ?></p>
+      </footer>
+
+    </div> <!-- /container -->
+  </body>
+</html>

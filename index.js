e6730efb83e4f9ec5141be0fbf6b1b502ee6fc48
zamankazi <kazi@programmer.net>
2015-06-10 18:11:34 -0400

    Add body parser package

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser     = require('body-parser'),
+    mongoose = require('mongoose');
 
 app.get('/', function(req,res) {
   res.send('hello world')
 })
 
 
-var server = app.listen(3000, '127.0.0.1')
+var server = app.listen(3000, '127.0.0.1', function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+
+});
 
 
 module.exports = server

bce2aac160a32621133999c86e958d1e2547e362
Stephen Hutchens <sbrhutchens@aol.com>
2015-06-11 17:26:19 -0400

    route for about page

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,12 @@
 
 module.exports = function(app) {
+
   app.get('/', function(req,res) {
     res.render('home')
+  })
+
+  app.get('/about', function(req,res) {
+    res.render('about')
   })
 
 }

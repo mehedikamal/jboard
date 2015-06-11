6d5e685e174f288e3eff6d77ec55f817a7b4748a
Stephen Hutchens <sbrhutchens@aol.com>
2015-06-10 20:46:56 -0400

    setting up for views integration

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var express = require('express'),
     app = express(),
     bodyParser     = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    ejs = require('ejs').renderFile;
+
+app.engine('html', ejs)
+app.set('view engine', 'html')
+app.use(express.static('public'))
 
 app.get('/', function(req,res) {
-  res.send('hello world')
+  res.render('home')
 })
 
 
 var server = app.listen(3000, '127.0.0.1', function(){
   var host = server.address().address;
-  var port = server.address().port;
+  var port = server.address().port; // why not use process.env.PORT here?
 
   console.log('Example app listening at http://%s:%s', host, port);
 

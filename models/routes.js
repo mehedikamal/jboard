a2d417dd15e2cb94a95e24db23f066aa1adc4bc8
Stephen Hutchens <sbrhutchens@aol.com>
2015-07-05 12:50:37 -0400

    nuke the entire project

--- models/routes.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  foo: function () {
-    return 'foo';
-  },
-  foo2: function () {
-    return 'foo2';
-  }
-}
--- views/views.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app,express) {
-  var ejs = require('ejs').renderFile
-
-  app.engine('html', ejs)
-  app.set('view engine', 'html')
-  app.use(express.static('public'))
-}

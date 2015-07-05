9b90c257d7215efa9f69150601c57ce27ab6aac6
Stephen Hutchens <sbrhutchens@aol.com>
2015-07-05 13:12:15 -0400

    adding posting functionality - just about finished

--- jboard.js
+++ jboard.js
@@ -1,19 +1,72 @@
+Posts = new Mongo.Collection('jobs')
+
+Router.configure({
+  layoutTemplate: 'main'
+})
+
+Router.route('/', function() {
+  this.render('home')
+})
+
+Router.route('/create-posting', function() {
+  this.render('create-posting')
+})
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.condensedJobs.helpers({
+    frontEndJobs: function() {
+      return Posts.find({ category: 'front-end javascript'}, {sort: {createdAt: -1}})
+    },
+    backEndJobs: function() {
+      return Posts.find({ category: 'back-end javascript'}, {sort: {createdAt: -1}})
+    },
+    isomorphic: function() {
+      return Posts.find({ category: 'isomorphic javascript'}, {sort: {createdAt: -1}})
+    },
+    miscellaneous: function() {
+      return Posts.find({ category: 'miscellaneous'}, {sort: {createdAt: -1}})
+    },
+    design: function() {
+      return Posts.find({ category: 'design'}, {sort: {createdAt: -1}})
+    },
+    salesMarketing: function() {
+      return Posts.find({ category: 'sales/marketing'}, {sort: {createdAt: -1}})
+    },
+    helpDesk: function() {
+      return Posts.find({ category: 'help desk/support'}, {sort: {createdAt: -1}})
+    },
+    systemAdmin: function() {
+      return Posts.find({ category: 'system admin'}, {sort: {createdAt: -1}})
     }
-  });
+  })
+  Template.createPosting.events({
+    "submit .create-posting-form": function(e) {
+      e.preventDefault();
+      var formData = $('.create-posting-form').serializeArray();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      Posts.insert({
+        createdAt: new Date(),
+        title: formData[0].value,
+        description: formData[1].value,
+        requirements: formData[2].value,
+        schedule: formData[3].value,
+        salary: formData[4].value,
+        category: formData[5].value,
+        name: formData[6].value,
+        headquarters: formData[7].value,
+        email: formData[8].value
+      })
+
+      if (typeof formData !== undefined) {
+        $('.status-notification').fadeIn();
+      }
     }
-  });
+  })
+
+  Template.body.events({
+    // place your delegated events here
+
+  })
 }
 
 if (Meteor.isServer) {

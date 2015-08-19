47d785d614e448feb33181dc57390e941bb0731d
Farisa1 <farisaahmedullah@gmail.com>
2015-08-18 20:19:18 -0400

    Added pages to sitemap.js page

--- server/sitemaps.js
+++ server/sitemaps.js
@@ -1,9 +1,16 @@
+
+
 sitemaps.add('/sitemap.xml', function() {
   // 'page' is reqired
   // 'lastmod', 'changefreq', 'priority' are optional.
   return [
-    { page: '/front_end_jobs', lastmod: new Date().getTime(), changefreq: 'daily' },
-    { page: '/back_end_jobs', lastmod: new Date().getTime(), changefreq: 'daily' },
-    { page: '/fullstack_jobs', lastmod: new Date().getTime(), changefreq: 'monthly', priority: 0.8 }
-  ];
+    { page: '/front_end_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8 },
+    { page: '/back_end_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8 },
+    { page: '/fullstack_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8 },
+    { page: '/miscellaneous_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8},
+    { page: '/design_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8},
+    { page: '/sales_marketing_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8},
+    { page: '/help_desk_support_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8},
+    { page: '/system_admin_jobs', lastmod: new Date().getTime(), changefreq: 'daily', priority: 0.8}
+];
 });

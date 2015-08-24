b06727c445bc8b1ffd3585dc670325961e7a01d2
Stephen Hutchens <sbrhutchens@aol.com>
2015-08-24 19:24:08 -0400

    Add quill editors to create posting page

--- client/events/create-posting.js
+++ client/events/create-posting.js
@@ -1,6 +1,12 @@
 Template.createPosting.events({
   "submit .create-posting-form": function(e) {
     e.preventDefault();
+    var description = $('input[name=description]'),
+        requirements = $('input[name=requirements]');
+
+        description.val($('.ql-editor').eq(0).html()) //capture description before serialization
+        requirements.val($('.ql-editor').eq(1).html()) //capture requirements before serialization
+
     var formData = $('.create-posting-form').serializeArray(),
         urlTitle = formData[0].value.replace(/\s+/g, '-');
 
@@ -11,3 +17,20 @@ Template.createPosting.events({
     }
   }
 })
+
+Template.createPosting.rendered = function() {
+  var editorOne = new Quill('#editor-one', {
+    modules: {
+      'multi-cursor': true,
+      'toolbar': { container: '#toolbar-one' }
+    },
+    theme: 'snow'
+  })
+  var editorTwo = new Quill('#editor-two', {
+    modules: {
+      'multi-cursor': true,
+      'toolbar': { container: '#toolbar-two' }
+    },
+    theme: 'snow'
+  })
+}

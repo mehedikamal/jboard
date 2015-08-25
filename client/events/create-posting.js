a823a7ad838ed93d51fbde9e2034f99b02d58f38
zamankazi <kazi@programmer.net>
2015-08-25 12:12:47 -0400

    Add indentation controls to job description text editor

--- client/events/create-posting.js
+++ client/events/create-posting.js
@@ -1,11 +1,9 @@
 Template.createPosting.events({
   "submit .create-posting-form": function(e) {
     e.preventDefault();
-    var description = $('input[name=description]'),
-        requirements = $('input[name=requirements]');
+    var description = $('input[name=description]');
 
         description.val($('.ql-editor').eq(0).html()) //capture description before serialization
-        requirements.val($('.ql-editor').eq(1).html()) //capture requirements before serialization
 
     var formData = $('.create-posting-form').serializeArray(),
         urlTitle = formData[0].value.replace(/\s+/g, '-');
@@ -23,13 +21,6 @@ Template.createPosting.rendered = function() {
     modules: {
       'multi-cursor': true,
       'toolbar': { container: '#toolbar-one' }
-    },
-    theme: 'snow'
-  })
-  var editorTwo = new Quill('#editor-two', {
-    modules: {
-      'multi-cursor': true,
-      'toolbar': { container: '#toolbar-two' }
     },
     theme: 'snow'
   })

4cb891c9fb393b4e860186e7847b4bddba7907aa
Stephen Hutchens <sbrhutchens@aol.com>
2015-08-30 16:53:00 -0400

    registering a helper for pretty strings

--- client/helpers/global-helpers.js
+++ client/helpers/global-helpers.js
@@ -3,3 +3,11 @@
    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
   return new Date(date).toLocaleDateString('en-us',options);
 });
+
+/* Pretty Strings */
+Template.registerHelper('prettyStrings', function(string) {
+  string = string.replace(/_/g, ' ');
+  string = string.charAt(0).toUpperCase() + string.substring(1);
+
+  return string
+})
